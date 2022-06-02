5950138927ad3edcc7ffce781eea79ae1ecee200
Snafu <snafu.alit@gmail.com>
2022-06-02 02:59:30 +0200

    ADD playRound() func

--- javascript.js
+++ javascript.js
@@ -10,3 +10,16 @@ function computerPlay(){
         return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.toLowerCase() === computerSelection.toLowerCase()) return "You both won! Yay!";
+    else if(playerSelection.toLowerCase() === "rock" && "Paper" === computerSelection) return "Paper beats Rock. Computer won!";
+    else if(playerSelection.toLowerCase() === "rock" && "Scissors" === computerSelection) return "Rock beats Scissors. Player won!";
+    else if(playerSelection.toLowerCase() === "scissors" && "Paper" === computerSelection) return "Scissors beats Paper. Player won!";
+    else if(playerSelection.toLowerCase() === "scissors" && "Rock" === computerSelection) return "Rock beats Scissors. Computer won!";
+    else if(playerSelection.toLowerCase() === "paper" && "Rock" === computerSelection) return "Paper beats rock. Player won!";
+    else if(playerSelection.toLowerCase() === "paper" && "Scissors" === computerSelection) return "Scissors beats Paper. Computer won!";
+    else return "Uh, oh. Something went wrong!";
+}
+
+console.log(playRound("Rock", computerPlay()));
